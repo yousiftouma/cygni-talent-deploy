e1125854ef2fbcd39fda7952931acdf8b32eb8db
lenkan <dlenksjo@gmail.com>
2021-03-15 23:42:06 +0100

    CD

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT ?? 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, "OK");
-  res.write(Buffer.from("Hello World 6!"));
+  res.write(Buffer.from("Hello World 7!"));
   res.end();
 });
 
