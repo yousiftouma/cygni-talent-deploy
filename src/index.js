a058831b6c5a6eddddf07b401c3f3d9a728874b7
lenkan <dlenksjo@gmail.com>
2021-03-27 01:08:10 +0100

    update build number in code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const http = require("http");
 
 const port = process.env.PORT ?? 8080;
+const buildNumber = process.env.BUILD_NUMBER || "n/a";
 
 const server = http.createServer((req, res) => {
   console.log(`Received request ${req.method.toUpperCase()} ${req.url}`);
   res.writeHead(200, "OK");
-  res.write(
-    Buffer.from("Hello World 10! Build number " + process.env.BUILD_NUMBER)
-  );
+  res.write(Buffer.from(`Hello World! Build number ${buildNumber}\n`));
   res.end();
 });
 
