c76d67a72b49fec2ec52f01f11f84e238f7fdd6b
lenkan <dlenksjo@gmail.com>
2021-04-01 12:48:19 +0200

    add helpful error message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-HOST=$DEPLOY_USER@$SERVER
+HOST=${DEPLOY_USER:?"Deployment user is required"}@${SERVER:?"Server is required"}
 DEPLOYMENT_NAME=app_$(date +%Y%m%d_%H%M%S)
 DEPLOYMENT_DIR=/opt/cygni/$DEPLOYMENT_NAME
 
